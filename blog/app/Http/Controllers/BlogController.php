e88abf50310df20ad7357e534bbff81d79a487f1
Nazmul haque <nazmulhaqued@gmail.com>
2017-11-17 23:54:46 +0600

    up to class 15
    Udemy blog up to class 15

---
+++ blog/app/Http/Controllers/BlogController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+class BlogController extends Controller
+{
+	protected $limit=3;
+
+    public function index(){
+
+    	$posts = Post::with('author')
+    			->latestFirst()
+    			->simplePaginate($this->limit);
+    	return view("blog.index", compact('posts'));
+    }
+    public function show(Post $post){
+    	return view("blog.show", compact('post'));
+    }
+}
